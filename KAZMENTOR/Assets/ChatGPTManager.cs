63fdf52033e95978cea766810111f7bcec829719
robbich <robbstarkst@gmail.com>
2024-06-22 10:43:10 +0500

    update
    added prompt to ai
    triggers for new scenes

--- KAZMENTOR/Assets/ChatGPTManager.cs
+++ KAZMENTOR/Assets/ChatGPTManager.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 using OpenAI;
 using UnityEngine.Events;
 
-public class ChatGPTManager : MonoBehaviour
-{
+public class ChatGPTManager : MonoBehaviour {
     public OnResponseEvent OnResponse;
 
     [System.Serializable]
-
-    public class OnResponseEvent : UnityEvent<string> {
-
-    }
+    public class OnResponseEvent : UnityEvent<string> { }
 
     private OpenAIApi openAI = new OpenAIApi("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");
     private List<ChatMessage> messages = new List<ChatMessage>();
+
+    // Создаем начальное сообщение для настройки контекста персонажа
+    private void Start() {
+        // Устанавливаем начальный промпт для AI
+        ChatMessage initialMessage = new ChatMessage();
+        initialMessage.Content = "You are Albert Einstein. Respond with knowledge about physics, relativity, and other scientific matters. Behave like a wise and knowledgeable scientist. Students of grades 5-7 will communicate with you. Give them moral lessons";
+        initialMessage.Role = "system";
+
+        messages.Add(initialMessage);
+    }
 
     public async void AskChatGPT(string newText) {
         ChatMessage newMessage = new ChatMessage();
@@ -30,7 +36,7 @@ public class ChatGPTManager : MonoBehaviour
 
         var response = await openAI.CreateChatCompletion(request);
 
-        if(response.Choices != null &&  response.Choices.Count > 0) {
+        if (response.Choices != null && response.Choices.Count > 0) {
             var chatResponse = response.Choices[0].Message;
             messages.Add(chatResponse);
 
@@ -38,17 +44,5 @@ public class ChatGPTManager : MonoBehaviour
 
             OnResponse.Invoke(chatResponse.Content);
         }
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
